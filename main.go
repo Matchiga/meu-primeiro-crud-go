7ed70fae61de9cfabc83354e5fd7920f71b02b80
Matchiga <matheuschiga1303@gmail.com>
2025-11-12 15:49:28 -0300

    Criando a camada de View

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/Matchiga/meu-primeiro-crud-go/src/controller"
 	"github.com/Matchiga/meu-primeiro-crud-go/src/controller/routes"
+	"github.com/Matchiga/meu-primeiro-crud-go/src/model/service"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -14,9 +16,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	//Init dependencies
+	service := service.NewUserDomainService()
+	userControler := controller.NewUserControllerInterface(service)
+
 	router := gin.Default()
 
-	routes.InitRoutes(&router.RouterGroup)
+	routes.InitRoutes(&router.RouterGroup, userControler)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
